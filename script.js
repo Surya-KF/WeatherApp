4fdb8f6380a3e289c1dff112bd39c1371fb3bebc
SuryaKF <suryakf04@gmail.com>
2024-09-25 19:32:44 +0530

    Updated the script file along with index file.

---
+++ script.js
@@ -0,0 +1,24 @@
+const userLocation = document.getElementById("userLocation"),
+      converter = document.getElementById("converter"),
+      weatherIcon = document.querySelector(".weatherIcon"),
+      temperature = document.querySelector(".temperature"),
+      feelsLike = document.querySelector(".feelsLike"),
+      description = document.querySelector(".description"),
+      date = document.querySelector(".date"),
+      city = document.querySelector(".city"),
+      HValue = document.getElementById("HValue"),
+      WValue = document.getElementById("WValue"),
+      SRValue = document.getElementById("SRValue"),
+      SSValue = document.getElementById("SSValue"),
+      CValue = document.getElementById("CValue"),
+      UVValue = document.getElementById("UVValue"),
+      PValue = document.getElementById("PValue"),
+      Forecast = document.querySelector(".Forecast");
+
+WEATHER_API_ENDPOINT = "";
+WEATHER_DATA_ENDPOINT = "";
+
+function findUserLocation(){
+    alert(1);
+ }
+
